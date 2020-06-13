78d5dc973b57aeca8b55506357ab196835ce45e8
Thibault FAGUE <faguethibault@gmail.com>
2020-06-13 11:56:38 +0200

    Add web app

---
+++ WebApplication/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BusinessLayer;
+using Model.Entities;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListEleves()
+        {
+            ViewBag.Message = "Liste d'élèves.";
+            List<Eleve> eleves = BusinessManager.Instance.GetAllEleve();
+            List<EleveViewModel> eleveViewModels = new List<EleveViewModel>();
+            foreach (Eleve e in eleves)
+            {
+                eleveViewModels.Add(new EleveViewModel { Nom = e.Nom, Prenom = e.Prenom, DateNaissance = e.DateNaissance });
+            }
+            return View(eleveViewModels);
+        }
+
+        public ActionResult AddEleve()
+        {
+            ViewBag.Message = "Ajouter un élève.";
+
+            return View();
+        }
+
+        public ActionResult AddAbsence()
+        {
+            ViewBag.Message = "Ajouter une absence.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication/Models/EleveViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class EleveViewModel
+    {
+        /// <summary>
+        /// Nom de l'élève
+        /// </summary>
+        public string Nom { get; set; }
+        /// <summary>
+        /// Prénom de l'élève
+        /// </summary>
+        public string Prenom { get; set; }
+        /// <summary>
+        /// Date de naissance de l'élève
+        /// </summary>
+        public DateTime DateNaissance { get; set; }
+    }
+}
